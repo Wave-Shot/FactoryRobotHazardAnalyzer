073f84adcd41cc77fed2871b419820378dc5f926
Wave-Shot <vsh.2509@gmail.com>
2026-02-03 15:23:54 +0530

    UC4: Add input validation using conditional logic

--- src/Program.java
+++ src/Program.java
@@ -9,14 +9,27 @@ public class Program {
         sc.nextLine();
         String machineryState = sc.nextLine();
 
+        if (armPrecision < 0.0 || armPrecision > 1.0) {
+            System.out.println("Error: Arm precision must be 0.0-1.0");
+            return;
+        }
+
+        if (workerDensity < 1 || workerDensity > 20) {
+            System.out.println("Error: Worker density must be 1-20");
+            return;
+        }
+
         double factor = 0;
 
         if (machineryState.equals("Worn")) factor = 1.3;
-        if (machineryState.equals("Faulty")) factor = 2.0;
-        if (machineryState.equals("Critical")) factor = 3.0;
+        else if (machineryState.equals("Faulty")) factor = 2.0;
+        else if (machineryState.equals("Critical")) factor = 3.0;
+        else {
+            System.out.println("Error: Unsupported machinery state");
+            return;
+        }
 
         double risk = ((1.0 - armPrecision) * 15.0) + (workerDensity * factor);
-
         System.out.println("Robot Hazard Risk Score: " + risk);
     }
 }
