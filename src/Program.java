8914cb355216ab877f1212bb71c4ae3b3da19e73
Wave-Shot <vsh.2509@gmail.com>
2026-02-03 15:25:27 +0530

    UC8: Final modular OOPS design with exception handling

--- src/Program.java
+++ src/Program.java
@@ -1,27 +1,21 @@
 import java.util.Scanner;
 
 public class Program {
-
-    public static double calculateHazardRisk(double armPrecision, int workerDensity, String machineryState) {
-
-        double factor = 0;
-
-        if (machineryState.equals("Worn")) factor = 1.3;
-        else if (machineryState.equals("Faulty")) factor = 2.0;
-        else if (machineryState.equals("Critical")) factor = 3.0;
-
-        return ((1.0 - armPrecision) * 15.0) + (workerDensity * factor);
-    }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        RobotHazardAuditor auditor = new RobotHazardAuditor();
 
-        double armPrecision = sc.nextDouble();
-        int workerDensity = sc.nextInt();
-        sc.nextLine();
-        String machineryState = sc.nextLine();
+        try {
+            double armPrecision = sc.nextDouble();
+            int workerDensity = sc.nextInt();
+            sc.nextLine();
+            String machineryState = sc.nextLine();
 
-        double risk = calculateHazardRisk(armPrecision, workerDensity, machineryState);
-        System.out.println("Robot Hazard Risk Score: " + risk);
+            double risk = auditor.calculateHazardRisk(armPrecision, workerDensity, machineryState);
+            System.out.println("Robot Hazard Risk Score: " + risk);
+
+        } catch (RobotSafetyException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
